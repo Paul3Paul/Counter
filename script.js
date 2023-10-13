123444abdd38c3113eaa6fd650aa52b614c5ee10
Paul3Paul <144097486+Paul3Paul@users.noreply.github.com>
2023-10-13 22:32:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-const p = document.getElementById('count')
+const p = document.getElementById('count');
 let c=0;
 function counterr1()
 {
-    c=c+1;
+   
+
+    if(c<=19)
+    {
+        c=c+1;
+    p.textContent=c;
+    console.log(c);
+    }
+    else
+    {
+        c=0;
+        p.textContent=c;
+    }
+}
+
+
+function counterr2()
+{
+    if(c>=1)
+    {
+    c= c-1 ;
     p.textContent=c;
     console.log(c);
 }
+else
+{
+    c=0;
+    p.textContent=c;
+}
+}
+function counterr3()
+{
+    c=0;
+    p.textContent=c;
+    console.log(c);
+}
+
